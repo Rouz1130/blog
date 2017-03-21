48ae63b0d9ea98795709f8feb957d0309c7bbc44
rouz majlessi <rmajlessi@gmail.com>
2017-03-21 00:31:15 -0700

    populate comment model with body that belongs to user and post, can track user and post with each comment

--- app/features/comment/model.js
+++ app/features/comment/model.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+    body: DS.attr('string'),
+    user: DS.belongsTo('user'),
+    post: DS.belongsTo('post')
 
 });
+// comment model has a body and belongs to user and post. That way you can track the user and post with each comment.

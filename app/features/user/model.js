c0ac8471482d53f8715a8eeeb6c7f4bb92cfdcd3
rouz majlessi <rmajlessi@gmail.com>
2017-03-21 00:34:11 -0700

    populate user model

--- app/features/user/model.js
+++ app/features/user/model.js
@@ -1,5 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  // uid, which is where we will store the unique identifier that is passed from Firebase after authenticating the user
+  uid: DS.attr('string'),
 
+  username: DS.attr('string'),
+  // username, the avatar which is the Twitter picture, and it has many posts and comments.
+  avatar: DS.attr('string'),
+  posts: DS.hasMany('post'),
+  comments: DS.hasMany('comment')
 });

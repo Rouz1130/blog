09b72d19353d763a389fa332ddaf92e1fef66b3a
rouz majlessi <rmajlessi@gmail.com>
2017-03-22 17:41:32 -0700

    populate edit-post component used to display each post and allow users logged in to edit their posts. computed property written tocheck verification if user can edit own post and allows for others not too edit users post.

--- app/features/components/add-comments/component.js
+++ app/features/components/add-comments/component.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // submit action revices author and body params and sends action to the route(posts) and clears the property
     actions:{
         submitComment(author, body){
             let post = this.get('post');
--- app/features/components/edit-post/component.js
+++ app/features/components/edit-post/component.js
@@ -1,4 +1,39 @@
 import Ember from 'ember';
+const {get, set} = Ember;
 
 export default Ember.Component.extend({
+  isEditing: false,
+  classNames: 'edit',
+
+  // computed property takes model and session objects passed in and checks to see is username matches. the purpose so users cant edit other peoples post only their own. isAllowed computed property will return true if everything matches and false if not.
+  isAllowed: Ember.computed('model.firstObject.user.username','session.currentUser.username', function(){
+    return get(this,'model.firstObject.user.username') === get(this,'session.currentUser.username');
+  }),
+
+  // sendAction sends everything to the route
+  actions:{
+    save(post){
+      let sessionName = get(this,'session.currentUser.username');
+      if(sessionName === post.get('user.username')){
+        // isEditing keeps track if article is being edited.
+        set(this, 'isEditing', false);
+        this.sendAction('save',post);
+
+      }
+      else{
+        alert('Sorry not authorized');
+      }
+
+    },
+    edit(){
+      set(this, 'isEditing', true);
+    },
+    delete(post){
+      this.sendAction('delete',post);
+      set(this,'isEditing',false);
+    },
+    createComment(author, body, post){
+      this.sendAction('createComment',author, body, post);
+    }
+  }
 });

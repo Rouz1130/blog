f0d7407d503b10fcf1a5ffac5df3b137a7417343
rouz majlessi <rmajlessi@gmail.com>
2017-03-22 20:02:05 -0700

    actions to create new post and then add it as an object to the user

--- app/features/new/route.js
+++ app/features/new/route.js
@@ -1,4 +1,39 @@
 import Ember from 'ember';
+import cleanURI from '../clean/util';
+import getOrCreateUser from '../get-or-create-user/util';
+
+const {get, set } = Ember;
 
 export default Ember.Route.extend({
+  // create a new post and then add it as an object to the user. The post gets saved first then the user
+  actions: {
+    save(title,body){
+      let user = null;
+      let titleURL= cleanURI(title);
+      let uid = get(this,'session.uid');
+      let date = new Date();
+      let post = this.store.createRecord('post',{
+        title: title,
+        body: body,
+        author: 'test',
+        titleURL:titleURL,
+        date: date
+      });
+
+      user = getOrCreateUser(uid,get(this,'session.currentUser.username'),
+      get(this,'session.currentUser.profileImageURL'),
+      this.store);
+      user.then((userData)=>{
+        userData.get('posts').addObject(post);
+        post.save().then(()=> {
+          return userData.save();
+        });
+
+      });
+
+      set(this, 'title','');
+      set(this, 'body','');
+      this.transitionTo('index');
+    }
+  }
 });
